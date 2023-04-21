7a0707cb315ac6ed4b7f4e68bdb08ad06e3b3e10
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-04-21 11:41:25 +0200

    colorize logger a bit more

--- src/server/lib/logger.ts
+++ src/server/lib/logger.ts
@@ -5,17 +5,36 @@ export type LogFunction = (message: string, ...args: any[]) => void;
 
 export type CreateLogger = (requestIdOrId: number | string) => LogFunction;
 
+const red = (str: string) => `\x1b[31m${str}\x1b[0m`;
+const green = (str: string) => `\x1b[32m${str}\x1b[0m`;
+const orange = (str: string) => `\x1b[33m${str}\x1b[0m`;
+const blue = (str: string) => `\x1b[34m${str}\x1b[0m`;
+
 export const makeCreateDefaultLogger = (f: WriteStream): CreateLogger => (requestIdOrId: number | string) =>
 	(...args: any[]) => {
 		const id = typeof requestIdOrId === 'number' ? `request #${requestIdOrId}` : requestIdOrId;
 
 		const parts = [`\x1b[94m[${id} at ${new Date().toISOString()}]\x1b[0m`, ...args.map((arg, i) => {
 			if (typeof arg === 'string' && i === 0) {
-				return arg.toLowerCase();
-			}
+				const str = arg.toLowerCase();
 
-			if (typeof arg === 'string') {
-				return arg;
+				if (str === 'error' || str.startsWith('fail')) {
+					return red(str);
+				}
+
+				if (str === 'warning') {
+					return orange(str);
+				}
+
+				if (str === 'success') {
+					return green(str);
+				}
+
+				if (str === 'info') {
+					return blue(str);
+				}
+
+				return str;
 			}
 
 			return inspect(arg, {depth: null, colors: true});
