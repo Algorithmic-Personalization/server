6e5c534d14a8de27046465f357e0d02d37ed192d
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-04-21 12:48:00 +0200

    allow participant arm to be 0 or 1 in create participant API

--- src/server/api-2/createParticipant.ts
+++ src/server/api-2/createParticipant.ts
@@ -2,6 +2,24 @@ import {type RouteDefinition} from '../lib/routeCreation';
 
 import {isParticipantRecord} from '../lib/participant';
 import Participant from '../models/participant';
+import {has} from '../../common/util';
+
+export type ParticipantData = {
+	arm: 'control' | 'treatment' | 0 | 1;
+	code: string;
+};
+
+export const isParticipantData = (record: Record<string, string | number>): record is ParticipantData =>
+	has('code')(record)
+	&& has('arm')(record)
+	&& typeof record.code === 'string'
+	&& record.code.length > 0
+	&& (
+		record.arm === 'control'
+		|| record.arm === 'treatment'
+		|| record.arm === 0
+		|| record.arm === 1
+	);
 
 export const createParticipantDefinition: RouteDefinition<Participant> = {
 	verb: 'post',
@@ -10,21 +28,27 @@ export const createParticipantDefinition: RouteDefinition<Participant> = {
 		const log = createLogger(req.requestId);
 		log('Received create participant request');
 
-		const {id: _unused, ...participantPayload} = req.body as Record<string, string>;
+		const {id: _unused, ...participantData} = req.body as Record<string, string>;
 
-		if (!isParticipantRecord(participantPayload)) {
+		if (!isParticipantData(participantData)) {
 			throw new Error('Invalid participant record');
 		}
 
 		const participantRepo = dataSource.getRepository(Participant);
 
-		if (await participantRepo.findOneBy({code: participantPayload.code})) {
+		if (await participantRepo.findOneBy({code: participantData.code})) {
 			throw new Error('Participant with that code already exists, use the update endpoint (PUT method) if you want to update it');
 		}
 
 		const participantEntity = new Participant();
 
-		Object.assign(participantEntity, participantPayload);
+		if (participantData.arm === 0) {
+			participantData.arm = 'control';
+		} else if (participantData.arm === 1) {
+			participantData.arm = 'treatment';
+		}
+
+		Object.assign(participantEntity, participantData);
 
 		return participantRepo.save(participantEntity);
 	},
