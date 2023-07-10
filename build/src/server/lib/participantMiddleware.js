4448f772042ca7460ec1f30cccad36339354cd4d
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-07-10 15:28:32 +0200

    forget about events that are ill-constructed

--- build/src/server/lib/participantMiddleware.js
+++ build/src/server/lib/participantMiddleware.js
@@ -5,14 +5,28 @@ const createParticipantMiddleWare = (createLogger) => (req, res, next) => {
     const log = createLogger(req.requestId);
     const participantCode = req.headers['x-participant-code'];
     log('checking participant code:', participantCode);
+    // Yes, sending a 200 response is intentional
+    // it is ugly, but clients will retry sending events to the server
+    // indefinitely until they get a 200 response from the server.
+    // And the mechanism was not designed in a way that lets the server
+    // tell clients that they should just give up.
+    // So in the cases where we know the request is invalid, we
+    // send a 200 response with a failure message so that clients
+    // can stop retrying.
+    const bail = () => {
+        res.status(200).json({
+            kind: 'Failure',
+            message: 'Invalid participant code',
+        });
+    };
     if (typeof participantCode !== 'string') {
-        log('Participant code is not a string');
-        res.status(401).json({ kind: 'Failure', message: 'Invalid participant code header', code: 'NOT_AUTHENTICATED' });
+        log('error', 'participant code is not a string:', participantCode);
+        bail();
         return;
     }
     if (!participantCode) {
-        log('participant code is empty');
-        res.status(401).json({ kind: 'Failure', message: 'Missing participant code header', code: 'NOT_AUTHENTICATED' });
+        log('error', 'participant code is empty');
+        bail();
         return;
     }
     req.participantCode = participantCode;
