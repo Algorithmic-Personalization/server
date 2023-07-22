79dc3da50baa46184295ec36465a7ab8bcb4f271
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-07-22 15:24:09 +0200

    parallel activation is red, good

--- src/integration-tests/activateExtension.test.ts
+++ src/integration-tests/activateExtension.test.ts
@@ -25,8 +25,12 @@ describe('activateExtension', () => {
 		await db.tearDown();
 	});
 
-	it('should activate the extension for a participant', async () => {
-		const activityNotifier = createMockParticipantActivityNotifier();
+	const createActivationDependencies = async (
+		activityNotifier: ParticipantActivityNotifier,
+	) => {
+		const participant = await db.createParticipant();
+		const session = await db.createSession(participant);
+		const event = await db.createEvent(session);
 
 		const activateExtension = createActivateExtension({
 			dataSource: db.dataSource,
@@ -34,11 +38,47 @@ describe('activateExtension', () => {
 			log: jest.fn(),
 		});
 
-		const participant = await db.createParticipant();
-		const session = await db.createSession(participant);
-		const event = await db.createEvent(session);
+		return {participant, event, activateExtension, activityNotifier};
+	};
+
+	it('should activate the extension for a participant', async () => {
+		const activityNotifier = createMockParticipantActivityNotifier();
+
+		const {
+			participant,
+			event,
+			activateExtension,
+		} = await createActivationDependencies(
+			activityNotifier,
+		);
 
 		await activateExtension(event, participant);
+
+		expect(activityNotifier.notifyActive).toHaveBeenCalledTimes(1);
+	});
+
+	it('should activate the extension only once for a participant', async () => {
+		const activityNotifier = createMockParticipantActivityNotifier();
+		const participant = await db.createParticipant();
+		const session = await db.createSession(participant);
+		const activateExtension = createActivateExtension({
+			dataSource: db.dataSource,
+			activityNotifier,
+			log: jest.fn(),
+		});
+
+		const activationRequests: Array<Promise<void>> = [];
+
+		for (let i = 0; i < 10; i++) {
+			const activationPromise = db.createEvent(session).then(
+				async event =>
+					activateExtension(event, participant),
+			);
+
+			activationRequests.push(activationPromise);
+		}
+
+		await Promise.allSettled(activationRequests);
 
 		expect(activityNotifier.notifyActive).toHaveBeenCalledTimes(1);
 	});
