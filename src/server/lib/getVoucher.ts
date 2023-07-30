f1babb24cad19a9372981bc84487158f6136e94d
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-07-30 14:59:50 +0200

    minimal version of getVoucher green

--- src/server/lib/getVoucher.ts
+++ src/server/lib/getVoucher.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
-import {DataSource} from 'typeorm';
+import {DataSource, IsNull} from 'typeorm';
 
 import {LogFunction} from './logger';
 import Voucher from '../models/voucher';
@@ -9,8 +9,26 @@ export type GetVoucherDependencies = {
 	dataSource: DataSource;
 };
 
-export const createGetVoucher = ({log}: GetVoucherDependencies) =>
+export const createGetVoucher = ({log, dataSource}: GetVoucherDependencies) =>
 	async (participantId: number): Promise<Voucher | undefined> => {
 		log('info', 'attempting to get voucher for participant', {participantId});
+
+		const repo = dataSource.getRepository(Voucher);
+
+		const voucher = await repo.findOne({
+			where: {
+				participantId: IsNull(),
+				deliveredAt: IsNull(),
+			},
+		});
+
+		if (voucher) {
+			voucher.participantId = participantId;
+			voucher.deliveredAt = new Date();
+			const saved = await repo.save(voucher);
+
+			return saved;
+		}
+
 		return undefined;
 	};
