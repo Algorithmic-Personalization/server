9813452a398a5699e5185a494c8217ef0cfda069
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-08-20 15:04:38 +0200

    update prod app URL

--- build/config.extension.js
+++ build/config.extension.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    'production-server-url': 'https://ytdpnl.fmdj.fr',
+    'production-server-url': 'https://personalization-server.csail.mit.edu/',
     'development-server-url': 'https://ytdpnl-dev.fmdj.fr',
 };
 //# sourceMappingURL=config.extension.js.map
