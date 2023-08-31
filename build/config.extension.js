c328625dd5b8c3a19c6906cb7e597ed4f96418bf
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-08-31 17:44:22 +0200

    fix bad prod server URL

--- build/config.extension.js
+++ build/config.extension.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
-    'production-server-url': 'https://personalization-server.csail.mit.edu/',
+    'production-server-url': 'https://personalization-server.csail.mit.edu',
     'development-server-url': 'https://ytdpnl-dev.fmdj.fr',
 };
 //# sourceMappingURL=config.extension.js.map
