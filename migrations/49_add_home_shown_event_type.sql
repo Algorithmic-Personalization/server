76464b4e611602a759671f66b603795f89a3ac9e
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-12-11 15:06:05 +0100

    add new event type

---
+++ migrations/49_add_home_shown_event_type.sql
@@ -0,0 +1,3 @@
+alter type EventType add value if not exists 'HOME_SHOWN';
+alter type ListType add value if not exists 'HOME_DEFAULT';
+alter type ListType add value if not exists 'HOME_REPLACEMENT_SOURCE';
