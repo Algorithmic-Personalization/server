b43f0680f3ae270247280f607d09a2443d64f422
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2024-04-16 12:46:00 +0200

    wip

--- src/server/api-2/sanityCheck.ts
+++ src/server/api-2/sanityCheck.ts
@@ -1,4 +1,5 @@
 import {type RouteDefinition} from '../lib/routeCreation';
+import {VideoListItem, ListType} from './../models/videoListItem';
 
 import Event, {EventType} from '../../common/models/event';
 
@@ -14,17 +15,69 @@ export const createSanityCheckDefinition: RouteDefinition<SanityCheck> = {
 		const log = createLogger(req.requestId);
 		log('received sanity check request');
 
+		const checkPhaseOneEvent = async (event: Event): Promise<boolean> => {
+			const items = await dataSource.getRepository(VideoListItem).find({
+				where: {
+					eventId: event.id,
+				},
+				order: {
+					listType: 'ASC',
+					position: 'ASC',
+				},
+			});
+
+			const def: VideoListItem[] = [];
+			const extSrc: VideoListItem[] = [];
+			const shown: VideoListItem[] = [];
+
+			for (const item of items) {
+				switch (item.listType) {
+					case ListType.HOME_DEFAULT:
+						def.push(item);
+						break;
+					case ListType.HOME_REPLACEMENT_SOURCE:
+						extSrc.push(item);
+						break;
+					case ListType.HOME_SHOWN:
+						shown.push(item);
+						break;
+					default:
+						throw new Error(`Unexpected list type: ${item.listType}`);
+				}
+			}
+
+			for (const item of def) {
+				const {position} = item;
+				if (shown.length <= position) {
+					log('error', `event ${event.id} has a HOME_DEFAULT item at position ${position} but only ${shown.length} HOME_SHOWN items`);
+					return false;
+				}
+
+				const shownItem = shown[position + 3];
+
+				if (shownItem.videoId !== item.videoId) {
+					log('error', `event ${event.id} has a HOME_DEFAULT item at position ${position} with video ${item.videoId} but HOME_SHOWN item at position ${position + 3} has video ${shownItem.videoId}`);
+					return false;
+				}
+			}
+
+			return true;
+		};
+
 		const eventRepo = dataSource.getRepository(Event);
 
 		const homeShownEvents = await eventRepo.find({
 			where: {
 				type: EventType.HOME_SHOWN,
 				phase: 1,
+				extensionVersion: '3.5.0',
 			},
 		});
 
+		const phaseOneOk = await Promise.all(homeShownEvents.map(checkPhaseOneEvent));
+
 		const nHomeShownEvents = homeShownEvents.length;
-		const nWeirdHomeShownEvents = 0;
+		const nWeirdHomeShownEvents = phaseOneOk.filter(ok => !ok).length;
 
 		return {
 			nHomeShownEvents,
